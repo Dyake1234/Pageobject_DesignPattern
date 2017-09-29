ad221464ab3bc6f9c7a3accbda3b79544354849b
Dyake1234 <jake.bachiller@igentechnologies.com>
2017-09-29 09:20:47 +0800

    Meron

--- GITPRACTICE/src/Earth.java
+++ GITPRACTICE/src/Earth.java
@@ -8,7 +8,7 @@ public class Earth {
 		System.out.println("storm earth and fire");
 		System.out.println("storm earth and fire");
 		// TODO Auto-generated methSod stub
-		System.out.println("haahabellssadasd");
+		System.out.println("haahabells");
 
 	}
 
